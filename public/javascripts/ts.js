56cb53a0ce4edc47a921f306e3ea990d3bc69953
Jacob Tardieu <jacob@tardieu.net>
2014-06-09 10:42:46 +0200

    Transform double click in simple in ts.js and fixed indentation

--- public/javascripts/ts.js
+++ public/javascripts/ts.js
@@ -1,9 +1,9 @@
-$( document ).ready(function(){
-$('#source').bind('dblclick',
-               function(){
-                   $(this).attr('contentEditable',true);
-               });
-$('#source').bind('keypress blur', function() {
-    $('#toto').text($(this).text());
-});
+$(document).ready(function () {
+    $('#source').bind('click',
+        function () {
+            $(this).attr('contentEditable', true);
+        });
+    $('#source').bind('keypress blur', function () {
+        $('#toto').text($(this).text());
+    });
 });
