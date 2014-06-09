358db51933117f0c7aa8f0ebc9a23e01634b1f4c
Jacob Tardieu <jacob@tardieu.net>
2014-06-09 18:36:22 +0200

    Fix missing charater and forbid catch the return key in text fields (issues #1 and #2). Closes #1. Closes #2.

--- public/javascripts/ts.js
+++ public/javascripts/ts.js
@@ -3,7 +3,12 @@ $(document).ready(function () {
         function () {
             $(this).attr('contentEditable', true);
         });
-    $('#source').bind('keypress blur', function () {
-        $('#toto').text($(this).text());
+    $('#source').bind('keypress keyup blur', function (e) {
+        if (e.keyCode == 13) { // Si c'est la touche entrée
+            this.blur(); // On enlève le focus
+            return false; // On termine la fonction pour pas que le retour à la ligne soit pris en compte
+        } else {
+            $('#toto').text($(this).text()); // Si ce n'est pas entrée on recopie le texte dans la boite d'en dessous
+        }
     });
 });
